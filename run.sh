e3fb3f724d3e7db758342a7c2406a75c64b2675c
Jonathan Burckel <jonathan.burckel@gmail.com>
2020-08-03 10:30:22 -1000

    Wrong name in shell script

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" = "dev" ] | [ "$1" = "DEV" ]; then
   export OAUTHLIB_INSECURE_TRANSPORT=1
-  uvicorn mpapi.main:app --reload
+  uvicorn app.main:app --reload
   unset OAUTHLIB_INSECURE_TRANSPORT
 else
-  uvicorn mpapi.main:app
+  uvicorn app.main:app
 fi
